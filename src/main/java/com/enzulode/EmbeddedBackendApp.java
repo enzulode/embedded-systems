8205ed2701f5acc81b62564fc1739b42f476246a
Vladislav Andreev <andreevvlad443@gmail.com>
2025-01-06 22:18:26 +0300

    initial commit

---
+++ src/main/java/com/enzulode/EmbeddedBackendApp.java
@@ -0,0 +1,12 @@
+package com.enzulode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedBackendApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmbeddedBackendApp.class, args);
+  }
+}
