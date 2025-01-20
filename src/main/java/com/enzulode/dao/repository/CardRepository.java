acb486b7db9c43c0e6c73df37df8bdc7261cfd16
Vladislav Andreev <andreevvlad443@gmail.com>
2025-01-20 10:46:00 +0300

    fix: dead code removal (deleteByCardId(UUID))

--- src/main/java/com/enzulode/dao/repository/CardRepository.java
+++ src/main/java/com/enzulode/dao/repository/CardRepository.java
@@ -2,7 +2,6 @@ package com.enzulode.dao.repository;
 
 import com.enzulode.dao.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,7 +11,4 @@ import java.util.UUID;
 public interface CardRepository extends JpaRepository<Card, Long> {
 
   Optional<Card> findByCardId(UUID cardId);
-
-  @Modifying
-  void deleteByCardId(UUID cardId);
 }
